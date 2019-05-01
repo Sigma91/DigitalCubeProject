898d8b039f5e4688690ce1ff1e94cef4b9f5c5fe
Nikola <Nikola@DESKTOP-GTJG1JQ>
2019-05-01 16:01:09 +0200

    Odradjene metode

--- DigitalCubeProject/src/glavni/TestKlasa.java
+++ DigitalCubeProject/src/glavni/TestKlasa.java
@@ -1,6 +1,7 @@
 package glavni;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,19 +13,21 @@ import org.hibernate.cfg.Configuration;
 import org.xml.sax.SAXException;
 
 import metode.XmlMetode;
+import model.Category;
+import model.Competitor;
+import model.Events;
 import model.Sport;
+import model.Tournament;
 
 public class TestKlasa {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
-		List<Sport> listaSportova = new ArrayList<Sport>();
+Competitor c = new Competitor();
 		
 		try {
-			listaSportova = XmlMetode.parseSportsXML();
-			for(Sport s: listaSportova) {
-				System.out.println(s.getId() + " " + s.getName());
-			}
+			c = XmlMetode.parseCompetitorXML("event-11427861.xml");
+			System.out.println(c.getId() +  " " + c.getName() + " " + c.getShort_name() + " " + c.getCountry_code());
 		} catch (ParserConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -36,7 +39,54 @@ public class TestKlasa {
 			e.printStackTrace();
 		}
 		
-		SessionFactory factory = new Configuration()
+Tournament t = new Tournament();
+		
+		try {
+			t = XmlMetode.parseTournamentXML("event-11427861.xml");
+			System.out.println(t.getId() +  " " + t.getName());
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+Events ev = new Events();
+		
+		try {
+			ev = XmlMetode.parseEventsXML("event-11427861.xml");
+			System.out.println(ev.getId() +  " " + ev.getUtc_scheduled());
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+Category cat = new Category();
+		
+		try {
+			cat = XmlMetode.parseCategoryXML("event-11427861.xml");
+			System.out.println(cat.getId() +  " " + cat.getName());
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		/*SessionFactory factory = new Configuration()
 				 .configure()
 				 .buildSessionFactory();
 
@@ -44,15 +94,15 @@ Session sesija = factory.openSession();
 sesija.beginTransaction();
 
 try{
-	listaSportova = XmlMetode.parseSportsXML();
- for(Sport s: listaSportova) {
+	c = XmlMetode.parseSportsXML();
+ for(Sport s: c) {
 	  sesija.save(s);
   }
 }catch (Exception e) {
 sesija.getTransaction().rollback();
 }
 
-sesija.close();
+sesija.close();*/
 
 
 
--- DigitalCubeProject/src/metode/XmlMetode.java
+++ DigitalCubeProject/src/metode/XmlMetode.java
@@ -2,7 +2,11 @@ package metode;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -15,7 +19,11 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import model.Category;
+import model.Competitor;
+import model.Events;
 import model.Sport;
+import model.Tournament;
 
 public class XmlMetode {
 	
@@ -46,6 +54,156 @@ public class XmlMetode {
 	         }
 	      }
 	      return sports;
+	   }
+	   
+	   public static Category parseCategoryXML(String xmlFajl) throws ParserConfigurationException, SAXException, IOException
+	   {
+	 
+	      Category category = new Category();
+	       
+	      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	      DocumentBuilder builder = factory.newDocumentBuilder();
+	      Document document = builder.parse(new File(xmlFajl));
+	      document.getDocumentElement().normalize();
+	      	 NodeList nList = document.getElementsByTagName("category");
+		         Node node = nList.item(0);
+		         if (node.getNodeType() == Node.ELEMENT_NODE)
+		         {
+		            Element eElement = (Element) node;
+		            category.setId(eElement.getAttribute("id"));
+		         }
+	         /*Nije nam potrebno jer se vuce automatski iz baze
+	          * NodeList pList = document.getElementsByTagName("sport");
+		         Node node1 = pList.item(0);
+		         if (node1.getNodeType() == Node.ELEMENT_NODE)
+		         {
+		            Element eElement = (Element) node1;
+		            category.setId_sport(eElement.getAttribute("id"));
+		         }*/
+		     NodeList qList = document.getElementsByTagName("venue");
+		         Node node2 = qList.item(0);
+		         if (node2.getNodeType() == Node.ELEMENT_NODE)
+		         {
+		            Element eElement = (Element) node2;
+		            category.setName(eElement.getAttribute("country_name"));
+		         }
+	         
+
+	      return category;
+	   }
+	   
+	   public static Competitor parseCompetitorXML(String xmlFajl) throws ParserConfigurationException, SAXException, IOException
+	   {
+	 
+	      Competitor competitor = new Competitor();
+	       
+	      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	      DocumentBuilder builder = factory.newDocumentBuilder();
+	      Document document = builder.parse(new File(xmlFajl));
+	      document.getDocumentElement().normalize();
+	      	 NodeList nList = document.getElementsByTagName("competitor");
+		         Node node = nList.item(0);
+		         if (node.getNodeType() == Node.ELEMENT_NODE)
+		         {
+		            Element eElement = (Element) node;
+		            competitor.setId(eElement.getAttribute("id"));
+		         }
+	         NodeList pList = document.getElementsByTagName("competitor");
+		         Node node1 = pList.item(0);
+		         if (node1.getNodeType() == Node.ELEMENT_NODE)
+		         {
+		            Element eElement = (Element) node1;
+		            competitor.setCountry_code(eElement.getAttribute("country_code"));
+		         }
+		     NodeList qList = document.getElementsByTagName("competitor");
+		         Node node2 = qList.item(0);
+		         if (node2.getNodeType() == Node.ELEMENT_NODE)
+		         {
+		            Element eElement = (Element) node2;
+		            competitor.setName(eElement.getAttribute("name"));
+		            		            	
+		            }		  	      
+		         
+		     NodeList sList = document.getElementsByTagName("competitor");
+		       Node node3 = sList.item(0);
+		         if (node3.getNodeType() == Node.ELEMENT_NODE)
+		         {
+		            Element eElement = (Element) node3;
+		            competitor.setShort_name(eElement.getAttribute("abbreviation"));
+		         }
+	         
+
+	      return competitor;
+	   }
+	   public static Tournament parseTournamentXML(String xmlFajl) throws ParserConfigurationException, SAXException, IOException
+	   {
+	 
+	      Tournament tournament = new Tournament();
+	       
+	      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	      DocumentBuilder builder = factory.newDocumentBuilder();
+	      Document document = builder.parse(new File(xmlFajl));
+	      document.getDocumentElement().normalize();
+	      	 NodeList nList = document.getElementsByTagName("tournament");
+		         Node node = nList.item(0);
+		         if (node.getNodeType() == Node.ELEMENT_NODE)
+		         {
+		            Element eElement = (Element) node;
+		            tournament.setId(eElement.getAttribute("id"));
+		         }
+	         /*Nije nam potrebno jer se vuce automatski iz baze
+	          * NodeList pList = document.getElementsByTagName("category");
+		         Node node1 = pList.item(0);
+		         if (node1.getNodeType() == Node.ELEMENT_NODE)
+		         {
+		            Element eElement = (Element) node1;
+		            tournament.setId_category(eElement.getAttribute("id"));
+		         }*/
+		     NodeList qList = document.getElementsByTagName("tournament");
+		         Node node2 = qList.item(0);
+		         if (node2.getNodeType() == Node.ELEMENT_NODE)
+		         {
+		            Element eElement = (Element) node2;
+		            tournament.setName(eElement.getAttribute("name"));
+		         }
+	         
+
+	      return tournament;
+	   }
+	   public static Events parseEventsXML(String xmlFajl) throws ParserConfigurationException, SAXException, IOException, ParseException
+	   {
+	 
+	      Events event = new Events();
+	       
+	      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	      DocumentBuilder builder = factory.newDocumentBuilder();
+	      Document document = builder.parse(new File(xmlFajl));
+	      document.getDocumentElement().normalize();
+	      	 NodeList nList = document.getElementsByTagName("fixture");
+		         Node node = nList.item(0);
+		         if (node.getNodeType() == Node.ELEMENT_NODE)
+		         {
+		            Element eElement = (Element) node;
+		            event.setId(eElement.getAttribute("id"));
+		         }
+	         NodeList pList = document.getElementsByTagName("fixture");
+		         Node node1 = pList.item(0);
+		         if (node1.getNodeType() == Node.ELEMENT_NODE)
+		         {
+		            Element eElement = (Element) node1;
+		            event.setUtc_scheduled(eElement.getAttribute("scheduled"));
+		            
+		            Timestamp ts=new Timestamp(System.currentTimeMillis());  
+	                Date date=ts;  
+	                System.out.println(date);     
+		            /*String input = "2017-09-23T13:15:00+00:00" ;
+		            java.sql.Timestamp ts = java.sql.Timestamp.valueOf( input ) ;	*/            		            
+		                
+		         }
+		     
+	         
+
+	      return event;
 	   }
 
 }
